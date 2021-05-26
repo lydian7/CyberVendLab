317f58bcd4717e9d957ac23e3845aa10a1819891
Ahmet Giritli <akgiritli@gmail.com>
2021-05-26 14:38:14 +0100

    Vending machine WIP, tests being written

--- src/main/java/Vender/VendingMachine.java
+++ src/main/java/Vender/VendingMachine.java
@@ -1,6 +1,8 @@
 package Vender;
 
 import products.Product;
+import schmeckles.Schmeckle;
+import schmeckles.SchmeckleReturn;
 import schmeckles.SchmeckleType;
 
 import java.util.ArrayList;
@@ -9,43 +11,15 @@ import java.util.HashMap;
 public class VendingMachine {
     
 
-    private HashMap<String, ArrayList<Product>> products;
-    private HashMap<SchmeckleType, ArrayList<SchmeckleType> > schmeckleVault;
+    private ArrayList<ProductRack> productRacks;
+    private ArrayList<Schmeckle> creditedScmheckles;
+    private SchmeckleReturn schmeckleReturn;
 
-    public VendingMachine(HashMap<String, ArrayList<Product>> products, HashMap<SchmeckleType, ArrayList<SchmeckleType>> schmeckleVault) {
-        this.products = products;
-        this.schmeckleVault = schmeckleVault;
+    public VendingMachine(ArrayList<ProductRack> productRacks, ArrayList<Schmeckle> creditedScmheckles, SchmeckleReturn schmeckleReturn) {
+        this.productRacks = productRacks;
+        this.creditedScmheckles = creditedScmheckles;
+        this.schmeckleReturn = schmeckleReturn;
     }
 
 
-    public HashMap<String, ArrayList<Product>> getProducts() {
-        return products;
-    }
-
-    public void setProducts(HashMap<String, ArrayList<Product>> products) {
-        this.products = products;
-    }
-
-    public HashMap<SchmeckleType, ArrayList<SchmeckleType>> getSchmeckleVault() {
-        return schmeckleVault;
-    }
-
-
-
-//set credit function
-    public void addToSchmeckleVault(SchmeckleType schmeckles) {
-        ArrayList<SchmeckleType> temp = schmeckleVault.get(schmeckles);
-
-        temp.add(schmeckles);
-
-        this.schmeckleVault.put(schmeckles, temp);
-    }
-
-    public void removeFromSchmeckleVault(SchmeckleType schmeckles){
-        ArrayList<SchmeckleType> tempRemove = schmeckleVault.get(schmeckles);
-
-        tempRemove.remove(schmeckles);
-
-        this.schmeckleVault.put(schmeckles, tempRemove);
-    }
 }
--- src/test/java/VenderTest/VendingMachineTest.java
+++ src/test/java/VenderTest/VendingMachineTest.java
@@ -1,4 +1,7 @@
 package VenderTest;
 
 public class VendingMachineTest {
+
+
+
 }
